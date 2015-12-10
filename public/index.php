87c1d5f0f5da9030b2fcbd4b2db430ba15eb4cb8
Paulo Chaves <paulochavesbr@gmail.com>
2015-12-10 15:52:05 -0600

    add entrypoint index.php

---
+++ public/index.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>REST API for Notes and Reminders</title>
+    <body>
+        <h1>It works!</h1>
+    </body>
+</html>
