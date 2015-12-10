8c737366229826baf39163b223686adc71bb7e57
Paulo Chaves <paulochavesbr@gmail.com>
2015-12-10 16:46:10 -0600

    add monolog config

--- app/settings.php
+++ app/settings.php
@@ -1,6 +1,11 @@
 <?php
 return [
     'settings' => [
-        
+
+        // monolog settings
+        'logger' => [
+            'name' => 'notes-api',
+            'path' => __DIR__ . '/../log/log-' . date('Y-m-d') . '.log',
+        ]
     ]
 ];
