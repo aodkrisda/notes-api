e2af19fcc355d4144f0d982ef5fab7ad497f272c
Paulo Chaves <paulochavesbr@gmail.com>
2015-12-10 23:58:37 -0600

    seed users table

--- app/database/seeds/UserSeed.php
+++ app/database/seeds/UserSeed.php
@@ -1,12 +1,20 @@
 <?php
+use App\Model\User;
 
-/**
- *
- */
 class UserSeed
 {
     public function run()
     {
-        echo "Run User seed\n";
+        User::create([
+            'name' => 'paul',
+            'email' => 'paul@email.com',
+            'password' => md5('paul')
+        ]);
+
+        User::create([
+            'name' => 'john',
+            'email' => 'john@email.com',
+            'password' => md5('john')
+        ]);
     }
 }
