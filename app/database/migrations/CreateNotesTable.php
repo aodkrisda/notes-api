4d7797ba812d1cdb98a86546f3c6271ff4ef3b0f
Paulo Chaves <paulochavesbr@gmail.com>
2015-12-11 00:40:17 -0600

    add migration for table notes

---
+++ app/database/migrations/CreateNotesTable.php
@@ -0,0 +1,31 @@
+<?php
+use Illuminate\Database\Capsule\Manager as DB;
+
+/**
+ * Migrations for users table
+ */
+class CreateNotesTable
+{
+    /**
+     * Run the migrations
+     *
+     * @return void
+     */
+    public function create()
+    {
+        DB::schema()->create('notes', function($table) {
+            $table->increments('id');
+            $table->integer('user_id')->unsigned();
+            $table->string('body');
+            $table->timestamps();
+
+            $table->foreign('user_id')
+                ->references('id')->on('users')
+                ->onDelete('cascade');
+        });
+    }
+
+    public function drop() {
+        DB::schema()->dropIfExists('notes');
+    }
+}
