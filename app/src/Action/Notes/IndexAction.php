16956fad1337982c7f5195baea8be9017ff35e0c
Paulo Chaves <paulochavesbr@gmail.com>
2015-12-13 18:20:40 -0600

    change index action constructor and find notes for the current user

--- app/src/Action/Notes/IndexAction.php
+++ app/src/Action/Notes/IndexAction.php
@@ -5,25 +5,27 @@ namespace App\Action\Notes;
 use Psr\Log\LoggerInterface;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Psr\Http\Message\ResponseInterface as Response;
-use App\Model\Note;
+use App\Model\User;
+use Slim\Container;
 
 /**
  * Return a list of notes of a user
  */
 final class IndexAction
 {
-    private $logger;
+    private $container;
 
-    function __construct(LoggerInterface $logger)
+    function __construct(Container $container)
     {
-        $this->logger = $logger;
+        $this->container = $container;
     }
 
     public function dispatch(Request $request, Response $response, $args)
     {
+        $user = $this->container->get('currentUser');
         return $response->write(json_encode([
             'message' => 'List of Notes',
-            'data' => Note::all()
+            'data' => User::findOrFail($user->userId)->notes
         ]));
     }
 };
