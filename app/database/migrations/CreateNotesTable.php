4709dc44da97b34fe797e2e58e222d62fd6d6336
Paulo Chaves <paulochavesbr@gmail.com>
2015-12-13 18:44:56 -0600

    change note body type to text

--- app/database/migrations/CreateNotesTable.php
+++ app/database/migrations/CreateNotesTable.php
@@ -16,7 +16,7 @@ class CreateNotesTable
         DB::schema()->create('notes', function($table) {
             $table->increments('id');
             $table->integer('user_id')->unsigned();
-            $table->string('body');
+            $table->mediumText('body');
             $table->timestamps();
 
             $table->foreign('user_id')
