838b34a573121dd9f19918ee20d1b37591699cfd
Paulo Chaves <paulochavesbr@gmail.com>
2015-12-13 18:19:45 -0600

    implement jwt verification

--- app/src/Middleware/Auth.php
+++ app/src/Middleware/Auth.php
@@ -4,9 +4,17 @@ namespace App\Middleware;
 
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Psr\Http\Message\ResponseInterface as Response;
+use Firebase\JWT\JWT;
+use Slim\App;
 
 class Auth
 {
+    private $app;
+
+    function __construct(App $app) {
+        $this->app = $app;
+    }
+
     /**
      * Handle authentication
      *
@@ -18,6 +26,36 @@ class Auth
      */
     public function __invoke(Request $request, Response $response, $next)
     {
+        $path = $request->getUri()->getPath();
+        if ($path && $path != 'login') {
+            $serverParams = $request->getServerParams();
+            $authHeader = isset($serverParams['HTTP_X_AUTHORIZATION']) ? $serverParams['HTTP_X_AUTHORIZATION']: null;
+            list($jwt) = sscanf($authHeader, 'Bearer %s');
+
+            if (!$jwt) {
+                return $response->withStatus(401)->write(json_encode([
+                    'message' => '401 Unauthorized'
+                ]));
+            }
+
+            try {
+                $settings = $this->app->getContainer()->get('settings');
+
+                $secretKey = base64_decode($settings->get('jwt')['key']);
+
+                $token = JWT::decode($jwt, $secretKey, [$settings->get('jwt')['algorithm']]);
+
+                // Get the user info and add to the container
+                $this->app->getContainer()['currentUser'] = function ($c) use ($token) {
+                    return $token->data; // user attributes
+                };
+            } catch (\Exception $e) {
+                return $response->withStatus(401)->write(json_encode([
+                    'message' => $e->getMessage()
+                ]));
+            }
+        }
+
         $response = $next($request, $response);
         return $response;
     }
