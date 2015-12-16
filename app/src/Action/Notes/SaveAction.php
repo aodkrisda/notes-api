15eb23d5156e3cbc67e20e990e482ccb1d18561c
Paulo Chaves <paulochavesbr@gmail.com>
2015-12-16 11:24:55 -0600

    add update method to notes SaveAction

--- app/src/Action/Notes/SaveAction.php
+++ app/src/Action/Notes/SaveAction.php
@@ -9,9 +9,9 @@ use Slim\Container;
 use App\Model\Note;
 
 /**
- * Add a new note for the current user
+ * Add a new note for the current user, or update a note if given its id
  */
-final class CreateAction
+final class SaveAction
 {
     private $container;
 
@@ -20,9 +20,18 @@ final class CreateAction
         $this->container = $container;
     }
 
+    /**
+     * Handle notes validation, creation and update
+     *
+     * @param  \Psr\Http\Message\ServerRequestInterface $request  PSR7 request
+     * @param  \Psr\Http\Message\ResponseInterface      $response PSR7 response
+     * @param  callable                                 $next     Next middleware
+     *
+     * @return \Psr\Http\Message\ResponseInterface
+     */
     public function dispatch(Request $request, Response $response, $args)
     {
-        $user = $this->container->get('currentUser');
+        $id = isset($args['id']) ? (int)$args['id']: null;
 
         $input = $request->getParsedBody();
 
@@ -30,14 +39,48 @@ final class CreateAction
 
         $validator->assert($input);
 
-        $note = Note::create([
+        if ($id === null) {
+            $note = $this->create($input);
+        } else {
+            $note = $this->update($input, $id);
+        }
+
+        return $response->write(json_encode([$note]));
+    }
+
+    /**
+     * Add a new note to the current user
+     *
+     * @param array request input
+     *
+     * @return App\Model\Note
+     */
+    public function create($input) {
+        $user = $this->container->get('currentUser');
+
+        return Note::create([
             'body' => $input['body'],
             'user_id' => $user->userId
         ]);
+    }
 
-        return $response->write(json_encode([
-            'message' => 'A new note has been added successfully',
-            'data' => [$note]
-        ]));
+    /**
+     * Update a specific note. Besides notes id, the user_id has to match, if it
+     * doesn't, throw not found exception
+     *
+     * @param array request input
+     * @param int   note's id
+     *
+     * @return App\Model\Note
+     */
+    public function update($input, $id) {
+        $user = $this->container->get('currentUser');
+
+        $note = Note::where('id', $id)->where('user_id', $user->userId)->firstOrFail();
+
+        $note->body = $input['body'];
+        $note->save();
+
+        return $note;
     }
 };
