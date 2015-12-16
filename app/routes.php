54494e4bd0c430b1cc90fc5bb9984d5efae668a4
Paulo Chaves <paulochavesbr@gmail.com>
2015-12-16 11:35:37 -0600

    add route to get a single note by id

--- app/routes.php
+++ app/routes.php
@@ -3,8 +3,9 @@
 $app->get('/', 'App\Action\HomeAction:dispatch')->setName('home');
 
 $app->get('/notes', 'Notes\IndexAction:dispatch')->setName('notes');
+$app->get('/notes/{id:[0-9]+}', 'Notes\IndexAction:dispatch')->setName('single-notes');
 $app->post('/notes', 'Notes\SaveAction:dispatch')->setName('add-notes');
-$app->put('/notes/{id:[0-9]+}', 'Notes\SaveAction:dispatch')->setName('add-notes');
+$app->put('/notes/{id:[0-9]+}', 'Notes\SaveAction:dispatch')->setName('update-notes');
 $app->delete('/notes', 'Notes\DeleteAction:dispatch')->setName('del-notes');
 $app->delete('/notes/{id:[0-9]+}', 'Notes\DeleteAction:dispatch')->setName('del-one-notes');
 
